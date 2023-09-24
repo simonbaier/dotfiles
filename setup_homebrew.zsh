ec7265d995a4f10343b1f210da0508f3248ae16d
Simon Baier <615713+simonbaier@users.noreply.github.com>
2023-09-24 14:18:12 -0400

    switched to Brewfile for brew installs

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,11 +4,16 @@ echo "\n<<< Starting Homebrew setup... >>>\n"
 
 /bin/bash -c "$(\curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat
+# Install packages from Brewfile ... create Brewfile using command > `brew bundle list --all`
+# running the following command will install all packages from Brewfile and create Brewfile.lock.json
+# make sure Brewfile and Brewfile.lock.json are in the same directory and add Brewfile.lock.json to .gitgnore
+brew bundle --verbose  
 
+# brew install httpie
+# brew install bat
+
+# brew install --cask --no-quarantine alfred
+# brew install --cask --no-quarantine dropbox
 # brew install --cask --no-quarantine google-chrome
-# brew install --cask --no-quarantine visual-studio-code
 # brew install --cask --no-quarantine iterm2
-brew install --cask --no-quarantine alfred
-
+# brew install --cask --no-quarantine visual-studio-code
