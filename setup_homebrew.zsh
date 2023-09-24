9786a7bd4d17e80ba1b657a6ce453f5cb18498cd
Simon Baier <615713+simonbaier@users.noreply.github.com>
2023-09-24 13:58:44 -0400

    ditto last comment

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -7,6 +7,8 @@ echo "\n<<< Starting Homebrew setup... >>>\n"
 brew install httpie
 brew install bat
 
-# brew install --cask google-chrome
-# brew install --cask visual-studio-code
-# brew install --cask iterm2
+# brew install --cask --no-quarantine google-chrome
+# brew install --cask --no-quarantine visual-studio-code
+# brew install --cask --no-quarantine iterm2
+brew install --cask --no-quarantine alfred
+
